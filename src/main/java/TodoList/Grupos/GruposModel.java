d9aff735d0396d38a3ef333a115bcb98b7b5501b
Dalton Dam <daltondam26@gmail.com>
2025-05-21 12:29:52 -0300

    Adcionando noargs e allargs constructor com o Lombok

--- src/main/java/TodoList/Grupos/GruposModel.java
+++ src/main/java/TodoList/Grupos/GruposModel.java
@@ -2,10 +2,16 @@ package TodoList.Grupos;
 
 import TodoList.Tarefas.TarefaModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "tb_grupos")
 public class GruposModel {
 
--- src/main/java/TodoList/Tarefas/TarefaModel.java
+++ src/main/java/TodoList/Tarefas/TarefaModel.java
@@ -2,12 +2,16 @@ package TodoList.Tarefas;
 
 import TodoList.Grupos.GruposModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Entity
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "tb_tarefa")
 public class TarefaModel {
 
@@ -24,6 +28,7 @@ public class TarefaModel {
     @ManyToOne
     @JoinColumn(name = "grupos_id") // Foreing Key
     private GruposModel grupos;
+
 
 
 
