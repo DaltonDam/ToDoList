74c4f34b2be463bbf0db5b4a6771279bb1399ed3
Dalton Dam <daltondam26@gmail.com>
2025-05-23 09:38:21 -0300

    feat: Cria as rotas da GruposController

--- src/main/java/TodoList/Grupos/GruposController.java
+++ src/main/java/TodoList/Grupos/GruposController.java
@@ -1,4 +1,29 @@
 package TodoList.Grupos;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("grupos")
 public class GruposController {
+
+    @GetMapping("/listar")
+    public String listarGrupo() {
+        return "Grupos listados com sucesso";
+    }
+
+    @PostMapping("/criar")
+    public String criarGrupo() {
+        return "Grupo criado";
+    }
+
+    @PutMapping("/alterar")
+    public String alterarGrupo() {
+        return "Grupo alterado";
+    }
+
+    @DeleteMapping("/deletar")
+    public String deletarGrupo() {
+        return "Grupo deletado";
+    }
+
 }
