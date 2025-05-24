3500c0ab80ea138404cb92385370bb30aec8ab5b
Dalton Dam <daltondam26@gmail.com>
2025-05-24 18:00:05 -0300

    feat: implementa o DTO e o Mapper na entidade Grupos

---
+++ src/main/java/TodoList/Grupos/GruposMapper.java
@@ -0,0 +1,29 @@
+package TodoList.Grupos;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class GruposMapper {
+
+    public GruposModel map(GruposDTO gruposDTO) {
+        GruposModel gruposModel = new GruposModel();
+        gruposModel.setId(gruposDTO.getId());
+        gruposModel.setNome(gruposDTO.getNome());
+        gruposModel.setTarefas(gruposDTO.getTarefas());
+
+        return gruposModel;
+
+    }
+
+    public GruposDTO map(GruposModel gruposModel) {
+        GruposDTO gruposDTO = new GruposDTO();
+        gruposDTO.setId(gruposModel.getId());
+        gruposDTO.setNome(gruposModel.getNome());
+        gruposDTO.setTarefas(gruposModel.getTarefas());
+
+        return gruposDTO;
+
+    }
+
+
+}
