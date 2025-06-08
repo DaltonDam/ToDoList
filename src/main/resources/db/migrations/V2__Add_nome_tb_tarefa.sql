d372c8014cacdb371e4e22ccbc7ad43d57448984
Dalton Dam <daltondam26@gmail.com>
2025-06-08 14:31:19 -0300

    fix: (migrations) change ALTER TABLE to CREATE TABLE for tb_tarefa

--- src/main/resources/db/migrations/V2__Add_nome_tb_tarefa.sql
+++ src/main/resources/db/migrations/V2__Add_nome_tb_tarefa.sql
@@ -1,4 +1,9 @@
 -- Migrations para adicionar a calouna NOME na tabela de tarefas
 
-ALTER TABLE tb_tarefa
-ADD COLUMN nome VARCHAR(255);
+--ALTER TABLE tb_tarefa
+--ADD COLUMN nome VARCHAR(255);
+
+CREATE TABLE tb_tarefa (
+    id SERIAL PRIMARY KEY
+    -- outras colunas básicas iniciais, se quiser
+);
